902a85fa79a08d2ba0ade3cbfdc95736ad06de48
Znerzi <diazrenz18@gmail.com>
2026-06-07 14:29:57 +0800

    fixed

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     $conn = new mysqli("localhost", "root", "", "applicants_db");
 
-    if ($conn->connect_error){
-        ("Connection failed: " . $conn->connect_error);
+    if ($conn -> connect_error){
+        ("Connection failed: ". $conn -> connect_error);
     }
 ?>
