16b96ba4ad032e8fffab87359c15cdc27212c5a9
Znerzi <diazrenz18@gmail.com>
2026-06-07 11:09:49 +0800

    database

--- db.php
+++ db.php
@@ -1,19 +1,10 @@
 <?php
-// Database Connection File
-// This file connects to the MySQL database
+// Database connection
 
-$servername = "localhost";
-$username = "root";
-$password = "";  // Default XAMPP password is empty
-$dbname = "applicants_db";
+$conn = new mysqli("localhost", "root", "", "applicants_db");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check if connection failed
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Connection successful
 ?>
