3e93c47e846a2cc6ce2a0f85c1634d80b00bb5e0
Znerzi <diazrenz18@gmail.com>
2026-06-08 13:21:56 +0800

    index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ include 'db.php';
 
 $message = "";
 
-if (isset($_GET['msg'])) {
-    if ($_GET['msg'] == 'success') {
-        $message = "<p class='success'>✓ Added successfully!</p>";
-    } elseif ($_GET['msg'] == 'updated') {
-        $message = "<p class='success'>✓ Updated successfully!</p>";
-    } elseif ($_GET['msg'] == 'deleted') {
-        $message = "<p class='success'>✓ Deleted successfully!</p>";
-    }
+$msgTypes = [
+    'success' => '✓ Added successfully!',
+    'updated' => '✓ Updated successfully!',
+    'deleted' => '✓ Deleted successfully!'
+];
+
+if (isset($_GET['msg']) && isset($msgTypes[$_GET['msg']])) {
+    $message = "<p class='success'>{$msgTypes[$_GET['msg']]}</p>";
 }
 
 $result = $conn->query("SELECT * FROM applicants");
@@ -27,7 +27,7 @@ $result = $conn->query("SELECT * FROM applicants");
 <div class="container">
     <h1>Applicants Management System</h1>
 
-    <?= $message; ?>
+    <?= $message ?>
 
     <a href="create.php" class="btn btn-add">+ Add New Applicant</a>
 
@@ -43,17 +43,15 @@ $result = $conn->query("SELECT * FROM applicants");
         <?php if ($result->num_rows > 0): ?>
             <?php while ($row = $result->fetch_assoc()): ?>
                 <tr>
-                    <td><?= $row['id']; ?></td>
-                    <td><?= $row['name']; ?></td>
-                    <td><?= $row['location']; ?></td>
-                    <td><?= $row['position']; ?></td>
-                        <td>
-                            <a href="read.php?id=<?= $row['id']; ?>" class="btn btn-add">View</a>
-
-                            <a href="update.php?id=<?= $row['id']; ?>" class="btn btn-edit">Edit</a>
-
-                            <a href="#" onclick="deleteWithUndo(<?= $row['id']; ?>)" class="btn btn-delete">Delete</a>
-                        </td>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['name'] ?></td>
+                    <td><?= $row['location'] ?></td>
+                    <td><?= $row['position'] ?></td>
+                    <td>
+                        <a href="read.php?id=<?= $row['id'] ?>" class="btn btn-add">View</a>
+                        <a href="update.php?id=<?= $row['id'] ?>" class="btn btn-edit">Edit</a>
+                        <a href="delete.php?id=<?= $row['id'] ?>" class="btn btn-delete">Delete</a>
+                    </td>
                 </tr>
             <?php endwhile; ?>
         <?php else: ?>
@@ -64,48 +62,7 @@ $result = $conn->query("SELECT * FROM applicants");
     </table>
 </div>
 
-<div id="toast" class="toast">
-    <span>Deleting in 5s...</span>
-    <button onclick="undoDelete()">Undo</button>
-</div>
-
-<script>
-let timer;
-let deleteId;
-let sec = 5;
-
-function deleteWithUndo(id) {
-    deleteId = id;
-    sec = 5;
-
-    document.getElementById("toast").style.display = "block";
-    updateText();
-
-    timer = setInterval(() => {
-        sec--;
-        updateText();
-
-        if (sec === 0) {
-            clearInterval(timer);
-            window.location.href = "delete.php?id=" + deleteId;
-        }
-    }, 1000);
-}
-
-function undoDelete() {
-    clearInterval(timer);
-    document.getElementById("toast").style.display = "none";
-}
-
-function updateText() {
-    document.querySelector("#toast span").innerText =
-        "Deleting in " + sec + "s...";
-}
-</script>
-
 </body>
 </html>
 
-<?php
-$conn->close();
-?>
+<?php $conn->close(); ?>
