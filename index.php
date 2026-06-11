ce8c992a104fc6e91452e5d72995a96b3be6cdc9
Znerzi <diazrenz18@gmail.com>
2026-06-11 08:39:36 +0800

    new search bar

--- index.php
+++ index.php
@@ -13,28 +13,25 @@ if (isset($_GET['msg']) && isset($msgTypes[$_GET['msg']])) {
     $message = "<p class='error'>{$msgTypes[$_GET['msg']]}</p>";
 }
 
-$row = $result = $conn -> query("SELECT * FROM applicants");
+$search = "";
+
+if (isset($_GET['search']) && !empty(trim($_GET['search']))) {
+    $search = $conn->real_escape_string($_GET['search']);
+
+    $sql = "SELECT * FROM applicants 
+            WHERE name LIKE '%$search%' 
+            OR location LIKE '%$search%' 
+            OR position LIKE '%$search%'";
+} else {
+    $sql = "SELECT * FROM applicants";
+}
+
+$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Applicant Management</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Applicant Management</h1>
-
-        <a href="create.php" class="btn btn-add">Add Applicant</a>
-
-        <?php echo $message; ?>
-
-        <table class="table">
-            <tr>
-                <th>ID</th>
                 <th>Name</th>
                 <th>Location</th>
                 <th>Position</th>
